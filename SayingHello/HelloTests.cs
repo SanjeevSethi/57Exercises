c655c1be3c35215ca6dd430899a21af537594cc6
sanjeevsethi <sanjeev.sethi01@gmail.com>
2016-10-19 13:45:07 +0100

    ddd

---
+++ SayingHello/HelloTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SayingHello
+{
+    [TestFixture]
+    public class HelloTests
+    {
+        [Test]
+        public void WhenAskedYourNameYourNameIsReturned()
+        {
+            var app = new HelloWorld();
+            string name = "sanj";
+            Assert.AreEqual(name, app.AskName(name));
+        }
+
+        [Test]
+        public void GivenYourNameReturnAGreetingWithNameConcatanated()
+        {
+            var app = new HelloWorld();
+            string name = "Sanj";
+            string greeting = "Hello, Sanj, nice to meet you!";
+            Assert.AreEqual(greeting, app.CreateGreeting(name));
+        }
+    }
+}
---
+++ SayingHello/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayingHello
+{
+    public class HelloWorld
+    {
+
+        public HelloWorld()
+        {
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello")
+
+        }
+
+        public string AskName(string name)
+        {
+            var name = Console.ReadLine("What is your name? ")
+            return name;
+        }
+
+        public string CreateGreeting(string name)
+        {
+            return string.Format("Hello, {0}, nice to meet you!", name);
+        }
+
+    }
+}
